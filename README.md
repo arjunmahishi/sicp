3e026096f70570bdd0f864ca7af0bdbc2d19d919
Arjun Mahishi <arjun.mahishi@gmail.com>
2021-11-15 10:21:26 +0530

    Add placeholders for all the exercises

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Solutions for excercises in the book [Structure and Interpretation of Computer programs](https://doc.lagout.org/programmation/Lisp/Scheme/SICP.pdf)
 
-This is a part of a study-group @last9
+This is a part of a study-group at [Last9](https://last9.io/)
--- session-1.scm
+++ session-1.scm
@@ -1,4 +1,4 @@
-; Exercise 1.1
+;;;;;;;;;;;;;;;;;;;;;;;;;; EXERCISE 1.1
 ; 10
 ; 12
 ; 8
@@ -11,10 +11,10 @@
 ; 6
 ; 16
 
-; Exercise 1.2
+;;;;;;;;;;;;;;;;;;;;;;;;;; EXERCISE 1.2
 (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 3))))) (* 3 (- 6 2) (- 2 7)))
 
-; Exercise 1.3
+;;;;;;;;;;;;;;;;;;;;;;;;;; EXERCISE 1.3 (incomplete)
 (define (sum_of_sqr x y)
   (+ (* x x) (* y y)))
 
@@ -28,4 +28,25 @@
 
 (fn 1 2 3)
 
-; Exercise 1.4
+;;;;;;;;;;;;;;;;;;;;;;;;;; EXERCISE 1.4
+; (define (a-plus-abs-b a b)
+;   ((if (> b 0) + -) a b))
+;
+; (if (> b 0) + -) -> this procedure returns the operator "+" if
+; b is positive or returns "-" if it's negative. This operator
+; is supplied to the outer procedure which has the operands "a" and "b"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;; EXERCISE 1.5
+; applicative order -> should go into a stack overflow
+; normal order -> should return 0
+
+;;;;;;;;;;;;;;;;;;;;;;;;;; EXERCISE 1.6
+
+;;;;;;;;;;;;;;;;;;;;;;;;;; EXERCISE 1.7
+; (define (good-enough? guess x)
+;   (< (abs (- (square guess) x)) 0.001))
+; the above procedure will cause a problem if the input is < 0.001
+;
+;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;; EXERCISE 1.8
