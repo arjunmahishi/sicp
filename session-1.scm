ef9600396d2bd347f0ca43045b4f47ccfa907ee9
Arjun Mahishi <arjun.mahishi@gmail.com>
2021-11-15 11:40:16 +0530

    Update

--- session-1.scm
+++ session-1.scm
@@ -29,9 +29,9 @@
 (fn 1 2 3)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;; EXERCISE 1.4
-; (define (a-plus-abs-b a b)
-;   ((if (> b 0) + -) a b))
-;
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
 ; (if (> b 0) + -) -> this procedure returns the operator "+" if
 ; b is positive or returns "-" if it's negative. This operator
 ; is supplied to the outer procedure which has the operands "a" and "b"
@@ -43,10 +43,28 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;; EXERCISE 1.6
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;; EXERCISE 1.7
-; (define (good-enough? guess x)
-;   (< (abs (- (square guess) x)) 0.001))
-; the above procedure will cause a problem if the input is < 0.001
-;
-;
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;; EXERCISE 1.8
+; the above procedure will cause a problem if the input is < 0.001
+; Alternative:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;; EXERCISE 1.8 (incomplete)
+(define (cube-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (cube-iter (improve guess x)
+               x)))
+
+; (define (improve guess x)
+;   (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (cube guess) x)) 0.001))
+
+(define (cube-root x)
+  (cube-iter 1.0 x))
+
